8a5dcb0d3213b9f7449ca89f5dc41ccf9252306c
p.cibiras@gmail.com <p.cibiras@gmail.com>
2023-01-20 22:58:12 +0200

    commit

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
 
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_numbers + password_symbols + password_letters
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    passw_entry.insert(0, password)
+
+    pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     email = email_entry.get()
     website = web_entry.get()
     password = passw_entry.get()
-    with open("data.txt", "a") as file:
-        file.write(f"{email} | {website} | {password}")
-        file.write('\n')
-    email_entry.delete(0,'end')
-    passw_entry.delete(0,'end')
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Ooops!", message="Please don't leave any fields blank")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email},"
+                               f"\n Password: {password} \n Is it ok to save?")
+
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"{email} | {website} | {password}")
+                file.write('\n')
+            web_entry.delete(0,'end')
+            passw_entry.delete(0,'end')
 # ---------------------------- UI SETUP ------------------------------- #
 
-from tkinter import *
+
 
 window = Tk()
 window.title("Password Generator")
@@ -33,13 +65,11 @@ web_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2, sticky=W+E)
 email_entry.insert(0, "some@email.com")
-passw_entry = Entry(width=21)
-passw_entry.grid(column=1, row=3, sticky=W+E)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky=W+E)
-generate_passw_button = Button(text="Generate Password")
+generate_passw_button = Button(text="Generate Password", command=generate_password)
 generate_passw_button.grid(column=2, row=3, sticky=W+E)
-
-print ("something")
+passw_entry = Entry(width=21)
+passw_entry.grid(column=1, row=3, sticky=W+E)
 
 window.mainloop()
